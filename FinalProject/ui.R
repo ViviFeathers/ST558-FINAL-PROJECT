9f05a3181ddcfd6942149c8f4a680aef08e038ce
ViviFeathers <vlfeathe@ncsu.edu>
2023-12-05 22:47:01 -0500

    initial EAD

--- FinalProject/ui.R
+++ FinalProject/ui.R
@@ -7,27 +7,130 @@
 #    http://shiny.rstudio.com/
 #
 
+
 library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(tidyverse)
+library(caret)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
-fluidPage(
+data_whole <- read_csv("ra_data.csv")
+data_whole$HighBloodPressure <- as.factor(data_whole$HighBloodPressure) 
+data_whole$Smoke <- as.factor(data_whole$Smoke)
+data_whole$Bronchitis <- as.factor(data_whole$Bronchitis) 
+data_whole$Melanoma <- as.factor(data_whole$Melanoma)
+data_whole$Remission <- as.factor(data_whole$Remission) 
+data_whole$Narcotic <- as.factor(data_whole$Narcotic)
+data_whole$PainMed <- as.factor(data_whole$PainMed) 
+data_whole$DmardMed <- as.factor(data_whole$DmardMed)
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+# read in RA data
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+ui <- dashboardPage(skin = "black",
+  
+  dashboardHeader(title = "Rheumatoid Arthritis Disease Remission Prediction"),
+  
+  ## Sidebar content
+  dashboardSidebar(sidebarMenu(
+    # First tab
+    menuItem("About",tabName = "About",icon = icon("info-sign", lib = "glyphicon")),
+    
+    # Second tab
+    menuItem("Data Exploration", tabName = "DataExploration",icon = icon("th-list", lib = "glyphicon"),
+             selectizeInput(
+               inputId ="select1",
+               "Select a Numeric Variable",
+               choices = c("Age", "PainScale", "FatigueScale", "PatientGlobal", "MDGlobal", "MdhaqScore", "BMI"), selected = "Age"
+             ),
+             
+             #numeric summary
+             radioButtons(
+               "RB1",
+               label="Type of Summary",
+               choices = list(
+                 "Mean",
+                 "Median")
+             ),
+             
+             menuSubItem("Summary Table", tabName = "Summary",icon = icon("play", lib = "glyphicon")),
+             
+             #plots
+             sliderInput(inputId = "Bin", "Bin Number on the Histogram",
+                         min = 10, max = 30, value = 21, step = 1),
+             menuSubItem("Histogram", tabName = "Plot",icon = icon("play", lib = "glyphicon")),
+             
+             #categorical summary
+             selectizeInput(
+               inputId ="select2",
+               "Select a Categorical Variable",
+               choices = c("HighBloodPressure", "Smoke", "Bronchitis", "Melanoma", "Narcotic", "PainMed", "DmardMed"), selected = "HighBloodPressure"
+             ),
+             
+             #bar chart
+             menuSubItem("Bar Chart", tabName = "barPlot",icon = icon("play", lib = "glyphicon")),
+             
+             #box plot
+             h5("Investigate the Selected Numeric"),
+             h5("Variable Distribution in Each Group"),
+             h5("of the Selected Categorical Variable"),
+             
+             menuSubItem("Box Chart", tabName = "boxPlot",icon = icon("play", lib = "glyphicon"))
+            )
+    
+    
+    
+    
+    
+    )),
+    
+  dashboardBody(
+      #About
+    tabItems(
+      tabItem(tabName = "About",
+              h3("Purpose: To predict Rheumatoid Arthritis(RA) remission based on patients' disease activities, RA treatments, physical condition and other diagnostic diseases."),
+              br(),
+              h3("Data Source: Harvard Medical School Rheumatoid Arthritis Database"),
+              h4("Data consist of 1581 observations and 14 predictors. The outcome is Remission with vaule 0 as no remission, and 1 means remission."),
+              h4("14 predictors are listed as below:"),
+              h4("Age: numeric, patient's age"),
+              h4("HighBloodPressure: Binary, 1 = Yes, 0 = No"),
+              br(),
+              h3("App functions:"),
+              h4("1. Exploratory data analysis of the dataset"),
+              h4("2. Compare the performance of three models in predicting whether or not the patients in the dataset have diabetes or not."),
+              h4("3. Predict the probability of a patient have remission by user input values"),
+              h4("4. Save the data used in modeling as a csv file.")
+             ),
+      
+      tabItem(tabName = "Summary",
+              h2("Summary Table of Numeric Variable"),
+              fluidRow(
+                       box(width = 5,  
+                       solidHeader = TRUE, collapsible = FALSE,
+                       tableOutput("table")))),
+      
+      tabItem(tabName = "Plot",
+              h2("Histogram Plot for Numeric Variable"),
+              fluidRow(
+                box(width = 8,  
+                    solidHeader = TRUE, collapsible = FALSE,
+                    plotOutput("dataPlot")))),
+      
+      tabItem(tabName = "barPlot",
+              h2("Bar Chart for Categorical Variable"),
+              fluidRow(
+                box(width = 8,  
+                    solidHeader = TRUE, collapsible = FALSE,
+                    plotOutput("dataPlot1")))),
+      
+      tabItem(tabName = "boxPlot",
+              h2("Box Plot of the Selected Numeric Variable Distribution Across Groups from the Selected Categorical Variable"),
+              fluidRow(
+                box(width = 8,  
+                    solidHeader = TRUE, collapsible = FALSE,
+                    plotOutput("dataPlot2"))))
+      
+      
+))
 )
--- server.R
+++ server.R
@@ -1,28 +1,91 @@
 #
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
+# Final Project
 #
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Vivi Feathers
+# 12/01/2023
 
 library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(tidyverse)
+library(caret)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
-function(input, output, session) {
+# read in RA data
+data_whole <- read_csv("ra_data.csv")
+data_whole$HighBloodPressure <- as.factor(data_whole$HighBloodPressure) 
+data_whole$Smoke <- as.factor(data_whole$Smoke)
+data_whole$Bronchitis <- as.factor(data_whole$Bronchitis) 
+data_whole$Melanoma <- as.factor(data_whole$Melanoma)
+data_whole$Remission <- as.factor(data_whole$Remission) 
+data_whole$Narcotic <- as.factor(data_whole$Narcotic)
+data_whole$PainMed <- as.factor(data_whole$PainMed) 
+data_whole$DmardMed <- as.factor(data_whole$DmardMed)
+data_whole$Age <- as.numeric(data_whole$Age)
+data_whole$PainScale <- as.numeric(data_whole$PainScale)
+data_whole$PatientGlobal <- as.numeric(data_whole$PatientGlobal)
+data_whole$FatigueScale <- as.numeric(data_whole$FatigueScale)
+data_whole$MDGlobal <- as.numeric(data_whole$MDGlobal)
+data_whole$MdhaqScore <- as.numeric(data_whole$MdhaqScore)
+data_whole$BMI <- as.numeric(data_whole$BMI)
 
-    output$distPlot <- renderPlot({
+server <- function(input, output, session) {
+  
+  
+  #get new data based on the selected numeric variable
+  getData <- reactive({
+    newData <- data_whole %>% select(!!sym(input$select1), Remission)
+  })
+  
+  #summary table
+  output$table <- renderTable({
+    #get new data 
+    newData <- getData()
+    
+    if(input$RB1=="Mean"){
+      newData %>% group_by(Remission) %>%
+        summarise(
+          Avg = round(mean(!!sym(input$select1)), 2),
+          Sd = round(sd(!!sym(input$select1)), 2))
+    }else if(input$RB1=="Median"){
+      newData %>% group_by(Remission) %>%
+        summarise(
+          Median = median(!!sym(input$select1)),
+          IQR = round(IQR(!!sym(input$select1)), 2))
+    }
+  })
+  
+  #Histogram 
+  output$dataPlot <- renderPlot({
+    #get new data 
+    newData <- getData()
+    h <- ggplot(newData, aes(x = !!sym(input$select1), fill = Remission))
+    h + geom_histogram(alpha=0.5, position = 'dodge', bins = input$Bin) + labs(x = input$select1, title = paste0("Histogram of ", input$select1, " Distribution Across Remission Groups"))
+})
+  
+  #get new data based on the selected categorical variable
+  getData1 <- reactive({
+    newData1 <- data_whole %>% select(!!sym(input$select2), Remission)
+  })
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
+  #bar chart 
+  output$dataPlot1 <- renderPlot({
+    #get new data 
+    newData1 <- getData1()
+    b <- ggplot(newData1, aes(x = !!sym(input$select2), fill = Remission))
+    b + geom_bar(alpha=0.5, position = 'dodge') + labs(x = input$select2, title = paste0("Bar Chart of ", input$select2, " Count Across Remission Groups"))
+  })
+  
+  #get new data based on the selected numeric and categorical variable
+  getData2 <- reactive({
+    newData2 <- data_whole %>% select(!!sym(input$select1), !!sym(input$select2))
+  })
+  #box plot
+  output$dataPlot2 <- renderPlot({
+    #get new data 
+    newData2 <- getData2()
+    x <- ggplot(newData2, aes(y = !!sym(input$select1), x = !!sym(input$select2), fill = !!sym(input$select2)))
+    x + geom_boxplot(alpha=0.5, adjust = 0.5) + labs(y = input$select1, title = paste0("Box Plot of ", input$select1, " Distribution Across ", input$select2," Groups"))
+  })
+  
 }
